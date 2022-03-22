e0f34a02d92810440789188b6e7c3d867d91b0ca
Dhanshri911 <dszingade911@gmail.com>
2022-03-22 16:24:24 +0530

    UC1:Added Starting Position

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		        int START_POSITION = 0;
+		        System.out.println(START_POSITION);
+		    }
+}
